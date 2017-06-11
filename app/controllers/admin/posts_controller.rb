c6999ca61f6ad466c8e1dcbe70cae8e53964e8f8
Francesco Boffa <fra.boffa@gmail.com>
2017-06-11 17:07:18 +0100

    Basic publishing interface

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,5 +2,31 @@ module Admin
   class PostsController < AdminController
     def index
     end
+
+    def new
+      render locals: {
+        post: Post.new
+      }
+    end
+
+    def show
+      post = Post.find(params[:id])
+
+      render locals: {
+        post: post
+      }
+    end
+
+    def create
+      post = Post.new(params.require(:post).permit(:kind, :body))
+
+      if post.save
+        redirect_to [:admin, post]
+      else
+        render :new, locals: {
+          post: post
+        }
+      end
+    end
   end
 end
