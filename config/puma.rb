8fdac392039270a899f3b7d7da00b89a8aaf49ae
Francesco Boffa <fra.boffa@gmail.com>
2017-06-11 14:07:02 +0100

    Puma configuration for production

--- config/puma.rb
+++ config/puma.rb
@@ -7,9 +7,11 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
+if ENV['RAILS_ENV'] == 'production'
+  bind 'unix:///home/me/site/tmp/sockets/puma.sock'
+else
+  port        ENV.fetch("PORT") { 3000 }
+end
 
 # Specifies the `environment` that Puma will run in.
 #
