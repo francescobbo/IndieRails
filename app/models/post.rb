088c03482c691c541370c1871427c886659c001f
Francesco Boffa <fra.boffa@gmail.com>
2017-06-12 09:59:54 +0100

    Render post body using markdown

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,12 @@ class Post < ApplicationRecord
                 venue read comics audio exercise food quotation recipe chicken]
 
   validates :kind, presence: true
+
+  def body=(value)
+    renderer = Redcarpet::Render::HTML.new({})
+    markdown = Redcarpet::Markdown.new(renderer)
+
+    self[:rendered_body] = markdown.render(value)
+    self[:body] = value
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when the body is changed' do
+    it 'renders body as markdown' do
+      post = Post.new
+      post.body = '_hello_ **world**'
+      expect(post.rendered_body.strip).to eq '<p><em>hello</em> <strong>world</strong></p>'
+    end
+  end
 end
