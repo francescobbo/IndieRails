6f4db3fd3e6517a45387e95c0673787855d377bf
Francesco Boffa <fra.boffa@gmail.com>
2017-06-13 20:54:48 +0100

    Meta tags in admin

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,9 +1,13 @@
 module Admin
   class AdminController < ApplicationController
-    before_action :ensure_admin
+    before_action :set_title, :ensure_admin
     layout 'admin'
 
     private
+
+    def set_title
+      set_meta_tags(title: 'Admin')
+    end
 
     def ensure_admin
       redirect_to admin_signin_path unless cookies.signed.encrypted[:admin] == true
