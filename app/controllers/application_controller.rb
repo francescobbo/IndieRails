9673904181c236467318bc7a0b703278a9b18ee4
Francesco Boffa <fra.boffa@gmail.com>
2017-06-13 12:00:08 +0100

    Advertise webmention endpoint in HTTP headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  after_action :add_webmention_endpoint
+
+  private
+
+  def add_webmention_endpoint
+    response.headers['Link'] = "<#{webmentions_url}>; rel=\"webmention\"" if response.status == 200
+  end
 end
