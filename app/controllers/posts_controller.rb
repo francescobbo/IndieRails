28ef5bdfa501b231e5bef344925a1f71c2be44a4
Francesco Boffa <fra.boffa@gmail.com>
2017-06-15 09:01:35 +0100

    Have some meta

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,17 @@
 class PostsController < ApplicationController
   def index
+    set_meta_tags({
+      title: "Home Page",
+      description: 'My personal home page'
+    })
   end
 
   def show
     post = Post.find(params[:id])
 
     set_meta_tags({
-      title: post.title.presence || "Status Update #{l(post.created_at, format: :short)}"
+      title: post.title.presence || "Status Update #{l(post.created_at, format: :short)}",
+      description: truncate(post.content, separator: ' ', omission: ' ', length: 150)
     })
 
     if post.deleted?
