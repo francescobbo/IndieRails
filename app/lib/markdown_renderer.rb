cdfda82f4d5fc75b8c6fe26dd6b92cdc27e90437
Francesco Boffa <fra.boffa@gmail.com>
2017-06-27 13:57:04 +0100

    Customized img tag from markdown

--- app/lib/markdown_renderer.rb
+++ app/lib/markdown_renderer.rb
@@ -3,5 +3,19 @@ require 'rouge'
 require 'rouge/plugins/redcarpet'
 
 class MarkdownRenderer < Redcarpet::Render::HTML
+  include ActionView::Helpers::AssetTagHelper
   include Rouge::Plugins::Redcarpet
+
+  def image(link, alt_text, title)
+    if title =~ /\A[a-f0-9-]{36}\z/i
+      image = Medium.where(id: title).first
+      if image
+        image_tag(image.file.url(:large), alt: image.default_alt, class: alt_text)
+      else
+        ""
+      end
+    else
+      image_tag(link, alt: alt_text, title: title)
+    end
+  end
 end
